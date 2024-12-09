3e04d1c4f7aa656ab799efdc6f66c573e606a7fa
BugHunter-X <askzenai@gmail.com>
2024-12-09 05:54:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// wrong usage of Tailwind directives
+<div class="bg-gray-100 p-4 rounded-lg shadow-md">
+  <h2 class="text-2xl font-bold mb-2">Error: Uncommon Tailwind CSS Bug</h2>
+  <p class="text-gray-700">This is a paragraph with some text that should be styled with Tailwind. But if you use a class name that is misspelled, does not exist in your Tailwind configuration, or is applied incorrectly, it won't be styled as expected.</p>
+</div>
+```
